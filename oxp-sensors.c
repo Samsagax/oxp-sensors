09e360d75200a641fda61772ca60ecd05cc0eba0
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2022-11-02 11:32:18 -0300

    Testing v4 driver for mainline

--- oxp-sensors.c
+++ oxp-sensors.c
@@ -24,24 +24,19 @@
 #include <linux/platform_device.h>
 #include <linux/processor.h>
 
+/* Handle ACPI lock mechanism */
+static u32 oxp_mutex;
+
 #define ACPI_LOCK_DELAY_MS	500
 
-/* Handle ACPI lock mechanism */
-struct lock_data {
-	u32 mutex;
-	bool (*lock)(struct lock_data *data);
-	bool (*unlock)(struct lock_data *data);
-};
-
-static bool lock_global_acpi_lock(struct lock_data *data)
+static bool lock_global_acpi_lock(void)
 {
-	return ACPI_SUCCESS(acpi_acquire_global_lock(ACPI_LOCK_DELAY_MS,
-								 &data->mutex));
+	return ACPI_SUCCESS(acpi_acquire_global_lock(ACPI_LOCK_DELAY_MS, &oxp_mutex));
 }
 
-static bool unlock_global_acpi_lock(struct lock_data *data)
+static bool unlock_global_acpi_lock(void)
 {
-	return ACPI_SUCCESS(acpi_release_global_lock(data->mutex));
+	return ACPI_SUCCESS(acpi_release_global_lock(oxp_mutex));
 }
 
 #define OXP_SENSOR_FAN_REG		0x76 /* Fan reading is 2 registers long */
@@ -51,21 +46,11 @@ static bool unlock_global_acpi_lock(struct lock_data *data)
 static const struct dmi_system_id dmi_table[] = {
 	{
 		.matches = {
-			DMI_EXACT_MATCH(DMI_BOARD_VENDOR,
-					"ONE-NETBOOK TECHNOLOGY CO., LTD."),
-		},
-	},
-	{
-		.matches = {
-			DMI_EXACT_MATCH(DMI_BOARD_VENDOR,
-					"ONE-NETBOOK"),
+			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONE XPLAYER"),
 		},
 	},
 	{},
-};
-
-struct oxp_status {
-	struct lock_data lock_data;
 };
 
 /* Helper functions to handle EC read/write */
@@ -75,31 +60,35 @@ static int read_from_ec(u8 reg, int size, long *val)
 	int ret;
 	u8 buffer;
 
+	if (!lock_global_acpi_lock())
+		return -EBUSY;
+
 	*val = 0;
 	for (i = 0; i < size; i++) {
 		ret = ec_read(reg + i, &buffer);
 		if (ret)
 			return ret;
-		(*val) <<= i * 8;
+		*val <<= i * 8;
 		*val += buffer;
 	}
-	return ret;
+
+	if (!unlock_global_acpi_lock())
+		return -EBUSY;
+
+	return 0;
 }
 
 static int write_to_ec(const struct device *dev, u8 reg, u8 value)
 {
-	struct oxp_status *state = dev_get_drvdata(dev);
 	int ret;
 
-	if (!state->lock_data.lock(&state->lock_data)) {
-		dev_warn(dev, "Failed to acquire mutex");
+	if (!lock_global_acpi_lock())
 		return -EBUSY;
-	}
 
 	ret = ec_write(reg, value);
 
-	if (!state->lock_data.unlock(&state->lock_data))
-		dev_err(dev, "Failed to release mutex");
+	if (!unlock_global_acpi_lock())
+		return -EBUSY;
 
 	return ret;
 }
@@ -116,7 +105,7 @@ static int oxp_pwm_disable(const struct device *dev)
 
 /* Callbacks for hwmon interface */
 static umode_t oxp_ec_hwmon_is_visible(const void *drvdata,
-					enum hwmon_sensor_types type, u32 attr, int channel)
+				       enum hwmon_sensor_types type, u32 attr, int channel)
 {
 	switch (type) {
 	case hwmon_fan:
@@ -138,34 +127,29 @@ static int oxp_platform_read(struct device *dev, enum hwmon_sensor_types type,
 	case hwmon_fan:
 		switch (attr) {
 		case hwmon_fan_input:
-			return read_from_ec(OXP_SENSOR_FAN_REG,
-					   2,
-					   val);
+			return read_from_ec(OXP_SENSOR_FAN_REG, 2, val);
 		default:
-			dev_dbg(dev, "Unknown attribute for type %d: %d\n", type, attr);
-			return -EOPNOTSUPP;
 		}
+		break;
 	case hwmon_pwm:
 		switch (attr) {
 		case hwmon_pwm_input:
-			ret = read_from_ec(OXP_SENSOR_PWM_REG,
-					   2, val);
+			ret = read_from_ec(OXP_SENSOR_PWM_REG, 2, val);
 			*val = (*val * 255) / 100;
 			return ret;
 		case hwmon_pwm_enable:
 			return read_from_ec(OXP_SENSOR_PWM_ENABLE_REG, 1, val);
 		default:
-			dev_dbg(dev, "Unknown attribute for type %d: %d\n", type, attr);
-			return -EOPNOTSUPP;
 		}
+		break;
 	default:
-		dev_dbg(dev, "Unknown sensor type %d.\n", type);
-		return -EOPNOTSUPP;
 	}
+	dev_dbg(dev, "Unknown attribute for type %d: %d\n", type, attr);
+	return -EOPNOTSUPP;
 }
 
 static int oxp_platform_write(struct device *dev, enum hwmon_sensor_types type,
-		u32 attr, int channel, long val)
+			      u32 attr, int channel, long val)
 {
 	switch (type) {
 	case hwmon_pwm:
@@ -183,22 +167,20 @@ static int oxp_platform_write(struct device *dev, enum hwmon_sensor_types type,
 			val = (val * 100) / 255;
 			return write_to_ec(dev, OXP_SENSOR_PWM_REG, val);
 		default:
-			dev_dbg(dev, "Unknown attribute for type %d: %d", type, attr);
-			return -EOPNOTSUPP;
+			break;
 		}
 	default:
-		dev_dbg(dev, "Unknown sensor type: %d", type);
-		return -EOPNOTSUPP;
 	}
-	return -EINVAL;
+	dev_dbg(dev, "Unknown sensor type: %d", type);
+	return -EOPNOTSUPP;
 }
 
 /* Known sensors in the OXP EC controllers */
 static const struct hwmon_channel_info *oxp_platform_sensors[] = {
 	HWMON_CHANNEL_INFO(fan,
-		HWMON_F_INPUT),
+			   HWMON_F_INPUT),
 	HWMON_CHANNEL_INFO(pwm,
-		HWMON_PWM_INPUT | HWMON_PWM_ENABLE),
+			   HWMON_PWM_INPUT | HWMON_PWM_ENABLE),
 	NULL,
 };
 
@@ -219,23 +201,19 @@ static int oxp_platform_probe(struct platform_device *pdev)
 	const struct dmi_system_id *dmi_entry;
 	struct device *dev = &pdev->dev;
 	struct device *hwdev;
-	struct oxp_status *state;
 
-	/* Have to check for AMD processor here */
+	/* Have to check for AMD processor here because DMI strings are the
+	 * same between Intel and AMD boards, the only way to tell them appart
+	 * is the CPU.
+	 * Intel boards seem to have different EC registers and values to
+	 * read/write.
+	 */
 	dmi_entry = dmi_first_match(dmi_table);
 	if (!dmi_entry || boot_cpu_data.x86_vendor != X86_VENDOR_AMD)
 		return -ENODEV;
 
-	state = devm_kzalloc(dev, sizeof(struct oxp_status), GFP_KERNEL);
-	if (!state)
-		return -ENOMEM;
-
-	state->lock_data.mutex = 0;
-	state->lock_data.lock = lock_global_acpi_lock;
-	state->lock_data.unlock = unlock_global_acpi_lock;
-
-	hwdev = devm_hwmon_device_register_with_info(dev, "oxpec", state,
-							&oxp_ec_chip_info, NULL);
+	hwdev = devm_hwmon_device_register_with_info(dev, "oxpec", NULL,
+						     &oxp_ec_chip_info, NULL);
 
 	return PTR_ERR_OR_ZERO(hwdev);
 }
@@ -272,6 +250,5 @@ module_init(oxp_platform_init);
 module_exit(oxp_platform_exit);
 
 MODULE_AUTHOR("Joaquín Ignacio Aramendía <samsagax@gmail.com>");
-MODULE_DESCRIPTION(
-	"Platform driver that handles ACPI EC of OneXPlayer devices");
+MODULE_DESCRIPTION("Platform driver that handles EC sensors of OneXPlayer devices");
 MODULE_LICENSE("GPL");
